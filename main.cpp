0e08355eebbc79a681c1a053ad82cb4a0bf7d206
j_delsol_catie <j.delsol@catie.fr>
2020-09-03 16:47:53 +0200

    update example and add the do_connect() function  and the temporary power_on() function

--- main.cpp
+++ main.cpp
@@ -15,29 +15,84 @@
  * limitations under the License.
  */
 #include "mbed.h"
+#include "common_functions.h"
+#include "CellularDevice.h"
+
+#define RETRY_COUNT 3
+
+static DigitalOut gsm_start(PB_4);
+NetworkInterface *iface;
+
+void gsm_power_on(void)
+{
+	printf("GSM: power Up\n");
+	// power off
+	gsm_start = 0;
+	printf("GSM: ready\n");
+	ThisThread::sleep_for(800);
+	gsm_start = 1;
+	printf("GSM: power key pressed\n");
+	printf("GSM: ready\n");
+	ThisThread::sleep_for(1250);
+	gsm_start = 0;
+	printf("GSM: release power key\n");
+	printf("GSM: wait ready\n");
+	ThisThread::sleep_for(10000);
+	printf("GSM: ready\n");
+	ThisThread::sleep_for(20);
+}
+
+
+/**
+ * Connects to the Cellular Network
+ */
+nsapi_error_t do_connect()
+{
+    nsapi_error_t retcode = NSAPI_ERROR_OK;
+    uint8_t retry_counter = 0;
+
+    while (iface->get_connection_status() != NSAPI_STATUS_GLOBAL_UP) {
+        retcode = iface->connect();
+        if (retcode == NSAPI_ERROR_AUTH_FAILURE) {
+            printf("\n\nAuthentication Failure. Exiting application\n");
+        } else if (retcode == NSAPI_ERROR_OK) {
+        	printf("\n\nConnection Established.\n");
+        } else if (retry_counter > RETRY_COUNT) {
+        	printf("\n\nFatal connection failure: %d\n", retcode);
+        } else {
+        	printf("\n\nCouldn't connect: %d, will retry\n", retcode);
+            retry_counter++;
+            continue;
+        }
+        break;
+    }
+    return retcode;
+}
 
 int main()
 {
     printf("Cellular example\n");
 
-    NetworkInterface *net = NetworkInterface::get_default_instance();
-    if (!net) {
-        printf("Error! No network inteface found.\n");
-        return -1;
-    }
+    gsm_power_on();
 
-    // TODO: Register the network status handler, and implement reconnection logic.
+    iface = NetworkInterface::get_default_instance();
 
-    int connect_err = net->connect();
-    if (connect_err) {
-        printf("Connection error: %d\n", connect_err);
-        return -1;
-    }
+    MBED_ASSERT(iface);
 
-    SocketAddress a;
-    net->get_ip_address(&a);
-    printf("IP: %s\n", a.get_ip_address());
+    // set cellular default parameters
+    iface->set_default_parameters();
 
-    net->disconnect();
-    printf("Done\n");
+    /* Attempt to connect to a cellular network */
+	if (do_connect() == NSAPI_ERROR_OK) {
+		SocketAddress a;
+		iface->get_ip_address(&a);
+		printf("IP: %s\n", a.get_ip_address());
+	}
+
+    if (iface->disconnect() != NSAPI_ERROR_OK) {
+		printf("\n\n disconnect failed.\n\n");
+	}
+
+    // TODO: turn the GSM module off
+
 }
